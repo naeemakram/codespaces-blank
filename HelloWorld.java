8af449ab2153ee92ea6d19bba006fd5e1fb01fe6
Naeem Akram <naeem.akram.malik@gmail.com>
2022-11-18 06:49:18 +0000

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package com.helloworld.testautomationtv;
+    
+public class HelloWorld
+{
+    public static void helloJohny(){
+        System.out.println("Hello Johny!");
+    }
+
+    public static void main(String[] args){        
+        System.out.println("Hellow World!");
+        helloJohny();
+    }
+}
